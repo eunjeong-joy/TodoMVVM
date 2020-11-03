ffb4d9abaedc66cabddad19435326f8e76d03644
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-03 23:50:26 +0900

    Task Data Class 추가, 초기 변수 설정

---
+++ app/src/main/java/com/todomvvm/data/Task.kt
@@ -0,0 +1,24 @@
+package com.todomvvm.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "tasks")
+data class Task @JvmOverloads constructor(
+    @ColumnInfo(name = "title") var title: String = "",
+    @ColumnInfo(name = "description") var description: String = "",
+    @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()
+){
+    @ColumnInfo(name = "completed") var isCompleted = false
+
+    val titleForList: String
+        get() = if (title.isNotEmpty()) title else description
+
+    val isActive
+        get() = !isCompleted
+
+    val isEmpty
+        get() = title.isEmpty() && description.isEmpty()
+}
