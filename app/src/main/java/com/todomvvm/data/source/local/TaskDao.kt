2d37a650ebe9b97ba8fe4acd3de09a66b2d714fa
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-04 17:19:45 +0900

    Task Dao 생성.

---
+++ app/src/main/java/com/todomvvm/data/source/local/TaskDao.kt
@@ -0,0 +1,31 @@
+package com.todomvvm.data.source.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.todomvvm.data.Task
+
+@Dao interface TaskDao {
+
+    @Query("SELECT * FROM Tasks")
+    fun getTasks(): List<Task>
+
+    @Query("SELECT * FROM Tasks WHERE entryid = :taskId")
+    fun getTaskById(taskId: String): Task?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTask(task: Task)
+
+    @Query("UPDATE tasks SET completed = :completed WHERE entryid = :taskId")
+    fun updateComplete(taskId: String, completed: Boolean)
+
+    @Query("DELETE FROM Tasks WHERE entryid = :taskId")
+    fun deleteTaskById(taskId: String): Int
+
+    @Query("DELETE FROM Tasks")
+    fun deleteTasks()
+
+    @Query("DELETE FROM Tasks WHERE completed = 1")
+    fun deleteCompletedTasks(): Int
+}
