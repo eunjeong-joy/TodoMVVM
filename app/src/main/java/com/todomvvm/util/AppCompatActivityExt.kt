b406a7db69608dbe5ff985dd74c79e3056112240
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-09 16:24:19 +0900

    Util 함수 추가.

--- app/src/main/java/com/todomvvm/util/AppCompatActivityExt.kt
+++ app/src/main/java/com/todomvvm/util/AppCompatActivityExt.kt
@@ -3,6 +3,12 @@ package com.todomvvm.util
 import androidx.annotation.IdRes
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import com.todomvvm.ViewModelFactory
 
 fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.()-> Unit) {
     setSupportActionBar(findViewById(toolbarId))
@@ -10,3 +16,18 @@ fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.()
         action()
     }
 }
+
+fun AppCompatActivity.replaceFragmentInActivity(fragment: Fragment, frameId: Int) {
+    supportFragmentManager.transact {
+        replace(frameId, fragment)
+    }
+}
+
+private inline fun FragmentManager.transact(action: FragmentTransaction.() -> Unit){
+    beginTransaction().apply {
+        action()
+    }.commit()
+}
+
+fun <T : ViewModel> AppCompatActivity.obtainViewModel(viewModelClass: Class<T>) =
+    ViewModelProviders.of(this, ViewModelFactory.getInstance(application)).get(viewModelClass)
