e9081645c0f568302b908b7059b81a75c4b83c24
Choi Eunjeong <chldmswjd55@gmail.com>
2020-11-17 10:50:07 +0900

    TasksListBindings 클래스 이름 변경, 속성값 변경,

--- app/src/main/java/com/todomvvm/tasks/TasksListBindings.kt
+++ app/src/main/java/com/todomvvm/tasks/TasksListBindings.kt
@@ -4,9 +4,9 @@ import android.widget.ListView
 import androidx.databinding.BindingAdapter
 import com.todomvvm.data.Task
 
-object TasksListBinding {
+object TasksListBindings {
 
-    @BindingAdapter("app:items")
+    @BindingAdapter("items")
     @JvmStatic fun setItems(listView: ListView, items: List<Task>) {
         with(listView.adapter as TasksAdapter) {
             replaceData(items)
