ec24d51499666c1393e1c8aac149d867b30bbd09
Choi Eunjeong <chldmswjd55@gmail.com>
2020-12-01 16:01:47 +0900

    Tasks Dao 파일 정리.

--- app/src/main/java/com/todomvvm/data/source/local/TasksDao.kt
+++ app/src/main/java/com/todomvvm/data/source/local/TasksDao.kt
@@ -1,31 +1,25 @@
 package com.todomvvm.data.source.local
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.todomvvm.data.Task
 
 @Dao interface TasksDao {
 
-    @Query("SELECT * FROM Tasks")
-    fun getTasks(): List<Task>
+    @Query("SELECT * FROM Tasks") fun getTasks(): List<Task>
 
-    @Query("SELECT * FROM Tasks WHERE entryid = :taskId")
-    fun getTaskById(taskId: String): Task?
+    @Query("SELECT * FROM Tasks WHERE entryid = :taskId") fun getTaskById(taskId: String): Task?
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertTask(task: Task)
+    @Insert(onConflict = OnConflictStrategy.REPLACE) fun insertTask(task: Task)
+
+    @Update
+    fun updateTask(task: Task): Int
 
     @Query("UPDATE tasks SET completed = :completed WHERE entryid = :taskId")
-    fun updateComplete(taskId: String, completed: Boolean)
+    fun updateCompleted(taskId: String, completed: Boolean)
 
-    @Query("DELETE FROM Tasks WHERE entryid = :taskId")
-    fun deleteTaskById(taskId: String): Int
+    @Query("DELETE FROM Tasks WHERE entryid = :taskId") fun deleteTaskById(taskId: String): Int
 
-    @Query("DELETE FROM Tasks")
-    fun deleteTasks()
+    @Query("DELETE FROM Tasks") fun deleteTasks()
 
-    @Query("DELETE FROM Tasks WHERE completed = 1")
-    fun deleteCompletedTasks(): Int
+    @Query("DELETE FROM Tasks WHERE completed = 1") fun deleteCompletedTasks(): Int
 }
